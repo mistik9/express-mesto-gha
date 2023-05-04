85e5703d6293ea043622308da841f7bb6bd6e3e3
mistik9 <mistik9@icloud.com>
2023-05-04 21:53:39 +0600

    fix BadrequestError

--- utils/errors/index.js
+++ utils/errors/index.js
@@ -1,8 +1,10 @@
 const AuthError = require('./AuthError');
-const BadRequest = require('./BadRequest');
+const BadRequestError = require('./BadRequestError');
 const ConflictError = require('./ConflictError');
 const ForbiddenError = require('./ForbiddenError');
 const NotFoundError = require('./NotFoundError');
 const ServerError = require('./ServerError');
 
-module.exports = { AuthError, BadRequest, ConflictError, ForbiddenError, NotFoundError, ServerError }
+module.exports = {
+  AuthError, BadRequestError, ConflictError, ForbiddenError, NotFoundError, ServerError,
+};
