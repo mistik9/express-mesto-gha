99fcdc888f9c1e4ce9aa2ce238f53b900d0d8336
mistik9 <mistik9@icloud.com>
2023-05-05 21:52:17 +0600

    fix routers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-// const router = require('./routes/index');
+
 const { NOT_FOUND } = require('./utils/constants');
 const {
   userRouter, cardRouter, signupRouter, signinRouter,
@@ -13,8 +13,6 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(express.json());
-
-// app.use(router);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   .then(console.log('DB is connected'))
@@ -28,7 +26,7 @@ app.use(auth);
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
-// app.use((req, res, next) => next(res.status(NOT_FOUND).send({ message: 'Страница не найден' })));
+app.use((req, res, next) => next(res.status(NOT_FOUND).send({ message: 'Страница не найден' })));
 app.use(errors());
 
 app.listen(PORT, () => {
